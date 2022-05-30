4336b907ae6b4ddf34a1c25b50207530d036ca24
aflores85 <aflores85@gmail.com>
2022-05-30 15:17:47 -0300

    agrego api card

--- app.py
+++ app.py
@@ -37,7 +37,10 @@ class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.String(80), nullable=False)
-    
+    foro_id = db.Column(db.Integer, db.ForeignKey('foro.id'), nullable=False)
+    foro = db.relationship('Foro', 
+    backref=db.backref('subjects', lazy=True))
+
     def __repr__(self):
         return '<Subject %r>' % self.title
 
@@ -46,6 +49,10 @@ class Post(db.Model):
     title = db.Column(db.String(80), nullable=False)
     content = db.Column(db.String(80), nullable=False)
     image_url = db.Column(db.String(180), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    subject = db.relationship('Subject', 
+    backref=db.backref('posts', lazy=True))
+
 
     def __repr__(self):
         return '<Post %r>' % self.title
@@ -68,19 +75,20 @@ def new_foro():
 
 def new_subject():
     subject_to_create = Subject(title=request.json['title'],
-                          content=request.json['content']
-                                                        )
+                          content=request.json['content'],
+                          foro_id=request.json['foro_id'])
 
     db.session.add(subject_to_create)
     db.session.commit()
     return jsonify({'message': 'Subject created successfully'})
 
 
-@app.route('/api/v1/newPost', methods=['POST'])
+@app.route('/api/v1/newpost', methods=['POST'])
 def new_post():
     post_to_create = Post(title=request.json['title'],
                           content=request.json['content'],
-                          image_url=request.json['imageURL'])
+                          image_url=request.json['imageURL'],
+                          subject_id=request.json['subjectid'])
 
     db.session.add(post_to_create)
     db.session.commit()
@@ -89,31 +97,28 @@ def new_post():
 
 @app.route('/api/v1/getforo', methods=['GET'])
 def get_foro():
-    foros = Foro.query.all() 
+    foros_Query = Foro.query.all() 
     foros_list = []
-    for foros in Foro:
+    for foros in foros_Query:
         foros_list.append(
-            {'title': Foro.title,
-             'id': Foro.id,
-             'content': Foro.content           
+            {'title': foros.title,
+             'id': foros.id,
+             'content': foros.content           
              })
     return jsonify({'posts': foros_list, 'message': 'Posts fetched successfully'})
 
 
 @app.route('/api/v1/getsubject', methods=['GET'])
 def get_subject():    
-    subject = Post.query.all()
+    subject_Query = Subject.query.all()
     subject_list = []
-    for subject in Subject:
+    for subject in subject_Query:
         subject_list.append(
-            {'title': Subject.title,
-             'id': Subject.id,
-             'content': Subject.content
+            {'title': subject.title,
+             'id': subject.id,
+             'content': subject.content
             })
     return jsonify({'posts': subject_list, 'message': 'Posts fetched successfully'})
-
-
-
 
 
 @app.route('/api/v1/getPosts', methods=['GET'])
@@ -130,10 +135,28 @@ def get_posts():
              })
     return jsonify({'posts': posts_list, 'message': 'Posts fetched successfully'})
 
+#los deletes
+@app.route('/api/v1/deletepost/<id>', methods=['DELETE'])
+def delete_posts(id):
+    query_post_to_delete = Post.query.filter_by(id=id).first()
+    db.session.delete(query_post_to_delete)
+    db.session.commit()
+    return jsonify({'message': 'Posts fetched successfully'})
+
+
+
+@app.route('/api/v1/deletesubject/<id>', methods=['DELETE'])
+def delete_subject(id):
+    query_subject_to_delete = Subject.query.filter_by(id=id).first()
+    db.session.delete(query_subject_to_delete)
+    db.session.commit()
+    return jsonify({'message': 'Subject fetched successfully'})
+
 
 #https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
 #export FLASK_APP=app
 #flask run
-#curl -X POST https://127.0.0.1/api/v1/newForo -H 'Content-Type: application/json' -d '{"title":"nuevo titulo","content":"contenido"}'
-
+#curl -X POST http://127.0.0.1:5000/api/v1/newforo -H 'Content-Type: application/json' -d '{"title":"nuevo titulo X","content":"contenido X"}'
+#curl -X POST http://127.0.0.1:5000/api/v1/newsubject -H 'Content-Type: application/json' -d '{"title":"nuevo subject","content":"contenido1","foro_id":"1"}'
+#curl -X POST http://127.0.0.1:5000/api/v1/newpost -H 'Content-Type: application/json' -d '{"title":"nuevo post","content":"contenido1","imageURL":"", "subjectid":"1"}'
 db.create_all()
